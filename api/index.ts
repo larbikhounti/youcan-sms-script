4d9e11b84749e7788a020c576d2ff7a752204535
mohamed khounti <larbikhounti@gmail.com>
2024-05-22 15:47:42 +0100

    first

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,15 @@
-const express = require("express");
+import express, { Request, Response } from 'express';
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.listen(3002, () => console.log("Server ready on port 3002."));
 
-module.exports = app;
+app.listen(3003, () => {
+    console.log(`Server running at http://localhost:3000`)
+  })
+
+app.get("/", (req : Request, res : Response) => {
+    res.send('test')
+})
+
+
+
