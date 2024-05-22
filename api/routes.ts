ba99f96dcbc53a0a6d3ecd7ffe976d919087c700
mohamed khounti <larbikhounti@gmail.com>
2024-05-22 16:56:42 +0100

    response change

--- api/routes.ts
+++ api/routes.ts
@@ -5,8 +5,8 @@ const router = Router()
 
 
 router.post('/webhook', (req : Request , res : Response)=> {
-   // console.log(req)
-    res.status(200).json(req.body)
+    console.log(req.body)
+    res.status(200).json("message : ok")
 })
 
 
