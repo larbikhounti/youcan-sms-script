f10eeb47025ac04a119b0fd830f5188cbb92a2c8
mohamed khounti <larbikhounti@gmail.com>
2024-05-22 18:14:52 +0100

    send message suing twilio

--- api/routes.ts
+++ api/routes.ts
@@ -1,11 +1,17 @@
 import { Request, Response, Router } from "express";
-
+const sendMessage = require('./twilio')
 const router = Router()
 
 
 
 router.post('/webhook', (req : Request , res : Response)=> {
     console.log(req.body)
+    try {
+        sendMessage(req.body)
+    } catch (error) {
+        console.log(error)
+    }
+    sendMessage()
     res.status(200).json("message : ok")
 })
 
---
+++ api/twilio.ts
@@ -0,0 +1,20 @@
+const Twilio = require("twilio");
+
+
+function sendMessage(info : any) {
+   //+12674588206
+const client = new Twilio("AC38e6ba2e6860a993de8b9ac58816cb5c",'12ab2c94ee3c15dd7b08a156779f2ed3')
+const messagearabic = `مرحبا ${info.customer.full_name} طلبك قيد المعالجة`
+
+
+client.messages
+  .create({
+    body: messagearabic,
+    from: '+13203010161',
+    // to: '+212642230300'
+    to: info.phone
+   })
+  .then(message => console.log(message.sid)); 
+}
+
+module.exports = sendMessage
